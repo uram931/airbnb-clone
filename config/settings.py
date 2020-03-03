db41a9d3a70b9599af7a1ce974289ea5b26492fe
uram931 <59157588+uram931@users.noreply.github.com>
2020-03-04 00:27:29 +0900

    how to take admin
    how to add data in admin server

--- config/settings.py
+++ config/settings.py
@@ -38,9 +38,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-PROJECT_APPS = [
-    "users.apps.UsersConfig",
-]
+PROJECT_APPS = ["users.apps.UsersConfig"]
 
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
