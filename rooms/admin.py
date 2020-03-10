e84061902ec4a894864aba1343c9b7baa6aac133
uram931 <59157588+uram931@users.noreply.github.com>
2020-03-10 21:24:04 +0900

    room_model
    .

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,12 +2,25 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-@admin.register(models.RoomType)
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
     pass
 
 
 @admin.register(models.Room)
 class Roomadmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ """
 
     pass
--- rooms/models.py
+++ rooms/models.py
@@ -22,7 +22,47 @@ class AbstractItem(core_models.TimeStampedModel):
 
 class RoomType(AbstractItem):
 
-    pass
+    """ RoomType Object Definition """
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Object Definition """
+
+    # 메타 클래스란 모델 내의 모든 class 안에 있는 class 이다
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """ Facility Object Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Object Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -44,8 +84,15 @@ class Room(core_models.TimeStampedModel):
     instant_book = models.BooleanField(default=False)
 
     # 다른 모델과 연결할 때 foreinkey 사용 일대다 관계 user와 여러개의 room를 고를수 있는 관계
+    # Room 모델이 USER에게 연결되었다는 의미(host=key),on_delete는 행동을 의미함,장고에게 Room으로 무엇을 할건지 말하는 것을 의미
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_tpye = models.ManyToManyField(RoomType, blank=True)  # 다대다 관계
+    room_tpye = models.ForeignKey(
+        RoomType, on_delete=models.SET_NULL, null=True
+    )  # 다대다 관계
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
 
+    # 파이썬에서는 모든 class를 str로 변경하여 읽는다. 아래의 내용은 admin 패널에서 클릭시 하위항목이 클릭한 대상의 이름과 일치하게 뜨게 만든다.
     def __str__(self):
         return self.name
