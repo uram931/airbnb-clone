9ea9127facb64919736dc831e5e16e5d4c5bc795
uram931 <59157588+uram931@users.noreply.github.com>
2020-03-13 20:12:14 +0900

    admin패널
    .

--- rooms/admin.py
+++ rooms/admin.py
@@ -10,12 +10,55 @@ class ItemAdmin(admin.ModelAdmin):
     pass
 
 
+# Roomadmin,ModelAdmin은  모델이 외부에서 보여지는 방식,안에서 보여지는 방식을 변경하는 역할을 한다.
 @admin.register(models.Room)
 class Roomadmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
-    pass
+    fieldsets = (
+        ("Basic Info", {"fields": ("name", "country", "city", "price")}),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths",)}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_tpye",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+    search_fields = ("^city", "^host__username")
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
 
 
 @admin.register(models.Photo)
